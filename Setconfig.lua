c97e39dcd1714f85cd39c6fd7afb2de210b48579
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-02 21:34:38 -0300

    Update Setconfig.lua

--- Setconfig.lua
+++ Setconfig.lua
@@ -16,13 +16,13 @@ local infosys = Window:AddTab({ Title = "info", Icon = "home" })
 local cratesys = Window:AddTab({ Title = "Create system", Icon = "list" })
 local copysys = Window:AddTab({ Title = "copy syskey", Icon = "list" })
 
-info:AddSection("how to use")
+local section = info:AddSection("how to use")
 info:AddParagraph({
     Title = " Create gui ",
     content = "1) you will put the name of the UI in the first textbox\n2) You will place the UI replacement in the second textbox\n3) You will choose the size of the UI in the Dropdown "
   })
 
-info:AddSection("how to conf")
+local section = info:AddSection("how to conf")
 info:AddParagraph({
     Title = " config key sys  ",
     content = "1) you will put your vertises or lootlabs link\nI recommend using linkvertise.com (much better)\ncreate a link with text if you use linkvertise.com, the message there must be short without spaces\nexample: ''key1739'' or ''word_word_word''\n\n2) in the second textbox put the script key"
