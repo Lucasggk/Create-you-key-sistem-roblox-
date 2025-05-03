c8d5b9287f14faa2914a5d9328870a0d4a1f817a
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-03 00:34:20 -0300

    Create Your key sistem.lua

---
+++ Your key sistem.lua
@@ -0,0 +1,7 @@
+local name = _G().uiname 
+local sname = _G().subname 
+local s1 = _G().sizeui1
+local s2 = _G().sizeui2 
+local ts = _G().tabsizeui 
+local kl = _G().keylink 
+local ck = _G().crrkey 
