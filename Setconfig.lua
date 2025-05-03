158df134f17f0094a149b136d3ca1a35e62766ef
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-02 22:51:29 -0300

    Update Setconfig.lua

--- Setconfig.lua
+++ Setconfig.lua
@@ -86,7 +86,102 @@ Dropdown2:OnChanged(function(value)
     elseif value == "medium: 150" then
         tabsize_ui = 150
     elseif value == "large: 180" then
+        tablocal Fluent = loadstring(Game:HttpGet("https://raw.githubusercontent.com/discoart/FluentPlus/refs/heads/main/release.lua", true))() 
+
+
+local Window = Fluent:CreateWindow({
+    Title = "create key system",
+    SubTitle = "made by Lucas",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(500, 350),
+    Acrylic = false,
+    Theme = "Dark",
+    Center = true,
+    IsDraggable = true
+})
+
+local infosys = Window:AddTab({ Title = "info", Icon = "home" })
+local cratesys = Window:AddTab({ Title = "Create system", Icon = "list" })
+local copysys = Window:AddTab({ Title = "copy syskey", Icon = "list" })
+
+local section = infosys:AddSection("how to use")
+infosys:AddParagraph({
+    Title = " Create gui ",
+    Content = " 1) you will put the name of the UI in the first textbox\n\n 2) You will place the UI replacement in the second textbox\n\n 3) You will choose the size of the UI in the Dropdown "
+  })
+
+local section = infosys:AddSection("how to conf")
+infosys:AddParagraph({
+    Title = " config key sys  ",
+    Content = " 1) you will put your vertises or lootlabs link\n\n I recommend using linkvertise.com (much better)\n\n create a link with text if you use linkvertise.com, the message there must be short without spaces\n\n example: ''key1739'' or ''word_word_word''\n\n 2) in the second textbox put the script key"
+  })
+
+local nome_ui = ""
+local subname_ui = ""
+local size_ui = ""
+local tabsize_ui = ""
+
+local section = cratesys:AddSection("ui Settings")
+
+cratesys:AddInput("", {
+    Title = "ui name",
+    Default = "",
+    Placeholder = "UI name here ",
+    Numeric = false,
+    Callback = function(Value)
+        nome_ui = Value
+    end
+})
+
+cratesys:AddInput("", {
+    Title = "subtitle ui name",
+    Default = "",
+    Placeholder = "Subname UI here ",
+    Numeric = false,
+    Callback = function(Value)
+        subnome_ui = Value
+    end
+})
+
+local Dropdown1 = cratesys:AddDropdown("", {
+    Title = "select ui size ", 
+    Values = {"small: 350, 250", "medium: 500, 350", "large: 650, 450" },
+    Multi = false,
+    Default = "medium: 500, 350"
+})
+
+Dropdown1:OnChanged(function(value)
+     if value == "small: 350, 250" then
+        size_ui = {350, 250}
+    elseif value == "medium: 500, 350" then
+        size_ui = {500, 350}
+    elseif value == "large: 650, 450" then
+        size_ui = {650, 450}
+     end
+        print("size_ui: ".. size_ui[1] ..", ".. size_ui[2])
+end)
+
+local Dropdown2 = cratesys:AddDropdown("", {
+    Title = "select TabWidth size", 
+    Values = {"small: 120", "medium: 150", "large: 180" },
+    Multi = false,
+    Default = "medium: 150"
+})
+
+Dropdown2:OnChanged(function(value)
+        if value == "small: 120" then
+        tabsize_ui = 120
+    elseif value == "medium: 150" then
+        tabsize_ui = 150
+    elseif value == "large: 180" then
         tabsize_ui = 180
+        end
+        print("tabsize_ui: ".. tabsize_ui)
+end)
+
+local section = cratesys:AddSection("key Settings")
+
+size_ui = 180
         end
         print("tabsize_ui: ".. tabsize_ui)
 end)
