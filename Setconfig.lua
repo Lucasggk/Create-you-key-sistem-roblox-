a1a5d7f1c9ed1b65e592b02e5315e65acadff899
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-02 22:08:59 -0300

    Update Setconfig.lua

--- Setconfig.lua
+++ Setconfig.lua
@@ -19,13 +19,13 @@ local copysys = Window:AddTab({ Title = "copy syskey", Icon = "list" })
 local section = infosys:AddSection("how to use")
 infosys:AddParagraph({
     Title = " Create gui ",
-    Content = "1) you will put the name of the UI in the first textbox\n\n2) You will place the UI replacement in the second textbox\n\n 3) You will choose the size of the UI in the Dropdown "
+    Content = " 1) you will put the name of the UI in the first textbox\n\n 2) You will place the UI replacement in the second textbox\n\n 3) You will choose the size of the UI in the Dropdown "
   })
 
 local section = infosys:AddSection("how to conf")
 infosys:AddParagraph({
     Title = " config key sys  ",
-    Content = "1) you will put your vertises or lootlabs link\n\nI recommend using linkvertise.com (much better)\n\ncreate a link with text if you use linkvertise.com, the message there must be short without spaces\n\nexample: ''key1739'' or ''word_word_word''\n\n2) in the second textbox put the script key"
+    Content = " 1) you will put your vertises or lootlabs link\n\n I recommend using linkvertise.com (much better)\n\n create a link with text if you use linkvertise.com, the message there must be short without spaces\n\n example: ''key1739'' or ''word_word_word''\n\n 2) in the second textbox put the script key"
   })
 
 local nome_ui = ""
